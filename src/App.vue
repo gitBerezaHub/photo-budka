67cdbd129e57de0861422415af76218b253012ee
Kirill Berezin <berezinkb06@gmail.com>
2025-04-04 11:57:22 +0700

    feat: remove standart header 2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script lang="ts" setup>
 import {onMounted} from "vue";
-import {initTelegramApp, isTelegramWebApp} from "./api/telegram-utils.ts";
-
 onMounted(() => {
-  if (isTelegramWebApp()) {
-    initTelegramApp();
+  const isTelegram = window.Telegram && window.Telegram.WebApp;
+
+  if (isTelegram) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const isExpanded = urlParams.get('tgWebAppExpand') === '1';
+
+    if (!isExpanded) {
+      const currentUrl = window.location.href;
+      const separator = currentUrl.includes('?') ? '&' : '?';
+      const newUrl = `${currentUrl}${separator}tgWebAppExpand=1`;
+
+      window.location.href = newUrl;
+    }
+
+      window.Telegram?.WebApp?.ready();
   }
 });
 </script>
